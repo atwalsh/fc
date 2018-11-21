57a35724bcfeea1922c5d4ac89291de464441135
Adam Walsh <adamtwalsh@gmail.com>
2018-11-21 15:06:31 -0500

    remove `created_at` and `updated_at` from BaseModel if not Postgres

--- {{cookiecutter.app_name}}/{{cookiecutter.app_slug}}/models/base_model.py
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_slug}}/models/base_model.py
@@ -8,6 +8,9 @@ class BaseModel(db.Model):
     Base model used for all models.
     """
     __abstract__ = True
+    id = db.Column(db.Integer, primary_key=True)
+    {% if cookiecutter.db == "PostgreSQL" %}
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.statement_timestamp())
     updated_at = db.Column(db.DateTime(timezone=True), server_default=func.statement_timestamp(),
                            onupdate=func.clock_timestamp())
+    {% endif %}
